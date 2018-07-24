1f1e6637baefdddfb30fac74da86842684118c33
edwinpaye <baratheonts@gmail.com>
2018-07-23 22:22:42 -0400

    actualizacion clase ListUsuarios

--- ListaEstudiantes/src/Ventana/ListUsuarios.java
+++ ListaEstudiantes/src/Ventana/ListUsuarios.java
@@ -3,30 +3,56 @@ package Ventana;
 import java.util.ArrayList;
 
 public class ListUsuarios extends Autenticacion{
-    
-//    private Autenticacion autent = new Autenticacion();
+
     private ArrayList<Usuario> list;
 
-    public ListUsuarios() {
-        this.list = new ArrayList<Usuario>();
+    public ListUsuarios(UserManager userManager) {
+        this.list = userManager.ShowUsers();
     }
-    
-    public ListUsuarios(ArrayList<Usuario> listUser){
-        this.list = listUser;
-    }
-    
-    public void AddUser(String name, String lastName, String age, String ocupation){
-        if (CheckString(name) && CheckString(lastName) && checkInt(age) && CheckString(ocupation)) {
-            list.add(new Usuario(name, lastName, age, ocupation, String.valueOf(list.size()+1)));
-        }
-    }
-    
+
     public ArrayList<Usuario> getActual(){
         return list;
     }
-    
+
     public void setLista(ArrayList<Usuario> listUsuario){
         this.list = listUsuario;
     }
+
+    public ArrayList<Usuario> searchAllUserData(String date){
+        ArrayList<Usuario> auxiliar = new ArrayList<Usuario>();
+        for (int i = 0; i < list.size(); i++) {
+            if (searchUserData(i, date)) {
+                auxiliar.add(list.get(i));
+            }
+        }
+        return auxiliar;
+    }
+
+    public boolean searchUserData(int posicion, String date){
+        if (compareName(posicion, date) || compareLastName(posicion, date) || 
+            compareAddress(posicion, date) || compareSalary(posicion, date) || compareID(posicion, date)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean compareName(int posicionUser, String date){
+        return list.get(posicionUser).getName().contentEquals(date);
+    }
+
+    public boolean compareLastName(int posicionUser, String date){
+        return list.get(posicionUser).getLastName().contentEquals(date);
+    }
+
+    public boolean compareAddress(int posicionUser, String date){
+        return list.get(posicionUser).getDirection().contentEquals(date);
+    }
+
+    public boolean compareSalary(int posicionUser, String date){
+        return list.get(posicionUser).getSueldo().contentEquals(date);
+    }
     
+    public boolean compareID(int posicionUser, String date){
+        return list.get(posicionUser).getAge().contentEquals(date);
+    }
 }
