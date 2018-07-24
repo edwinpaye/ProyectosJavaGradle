340c20ca2917748077954760b8ed18f7b8868325
edwinpaye <baratheonts@gmail.com>
2018-07-23 22:38:34 -0400

    actualizacion clase Main

--- ListaEstudiantes/src/Ventana/Main.java
+++ ListaEstudiantes/src/Ventana/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        new Controlador().Start("root", "root", "192.168.100.8");
+        new Controlador().Start("root", "root", "directionIp");
     }
     
 }
