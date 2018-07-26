b0e7770b0faac799dd313b040e42a3ffb749f5d8
edwinpaye <baratheonts@gmail.com>
2018-07-26 18:38:10 -0400

    actualizacion clase Usuario

--- ListaEstudiantes/src/Ventana/Usuario.java
+++ ListaEstudiantes/src/Ventana/Usuario.java
@@ -1,29 +1,36 @@
 package Ventana;
-
 public class Usuario {
-    
-    private String name, lastName,age, direction, sueldo;
-
+    private String id_user, position, name, lastName;
+    int salary;
+        
     public Usuario() { }
     
-    public Usuario(String n,String l,String a,String d, String sueldo) {
-        name = n;
-        lastName = l;
-        age = a;
-        direction = d;
-        this.sueldo = sueldo;
+    public Usuario(String id_user,String position,String name,String lastName, int salary) {
+        this.id_user = id_user;
+        this.position = position;
+        this.name = name;
+        this.lastName = lastName;
+        this.salary = salary;
     }
    
-    public String getSueldo(){
-        return sueldo;
-    }
-    
-    public void setSueldo(String sueldo){
-        this.sueldo = sueldo;
-    }
-    
     public String VerInfo() {
-        return "Nombre: "+name+" Apellido: "+lastName+" Edad: "+age+" Direccion: "+direction;
+        return "Id usuario: "+id_user+"\tCargo: "+position+"\tNombre: "+name+"\tApellido: "+lastName+"\tSalario: "+salary;
+    }
+    
+    public String getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(String id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
     }
 
     public String getName() {
@@ -42,109 +49,13 @@ public class Usuario {
         this.lastName = lastName;
     }
 
-    public String getAge() {
-        return age;
+    public int getSalary() {
+        return salary;
     }
 
-    public void setAge(String age) {
-        this.age = age;
+    public void setSalary(int salary) {
+        this.salary = salary;
     }
 
-    public String getDirection() {
-        return direction;
-    }
 
-    public void setDirection(String direction) {
-        this.direction = direction;
-    }
-    
-   
 }
-//public class Usuario {
-//
-//    private String name = "";
-//    private String lastName = "";
-//    private String age = "";
-//    private String Ocupation = "";
-//    private String numReg = "";
-//
-//    public Usuario(String name, String lastName, String age, String Ocupation, String numReg){
-//        this.name = name;
-//        this.lastName  = lastName;
-//        this.age = age;
-//        this.Ocupation = Ocupation;
-//        this.numReg = numReg;
-//    }
-//   
-//    /**
-//     * @return the name
-//     */
-//    public String getName() {
-//        return name;
-//    }
-//
-//    /**
-//     * @param name the name to set
-//     */
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    /**
-//     * @return the lastName
-//     */
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    /**
-//     * @param lastName the lastName to set
-//     */
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    /**
-//     * @return the age
-//     */
-//    public String getAge() {
-//        return age;
-//    }
-//
-//    /**
-//     * @param age the age to set
-//     */
-//    public void setAge(String age) {
-//        this.age = age;
-//    }
-//
-//    /**
-//     * @return the Ocupation
-//     */
-//    public String getOcupation() {
-//        return Ocupation;
-//    }
-//
-//    /**
-//     * @param Ocupation the Ocupation to set
-//     */
-//    public void setOcupation(String Ocupation) {
-//        this.Ocupation = Ocupation;
-//    }
-//
-//    /**
-//     * @return the numReg
-//     */
-//    public String getNumReg() {
-//        return numReg;
-//    }
-//
-//    /**
-//     * @param numReg the numReg to set
-//     */
-//    public void setNumReg(String numReg) {
-//        this.numReg = numReg;
-//    }
-//    
-// 
-//}
