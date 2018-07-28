e1c26b60db785380ba7ff3cf124a4a8c45708e6e
edwinpaye <baratheonts@gmail.com>
2018-07-28 01:41:04 -0400

    actualizacion clase UserManager

--- ListaEstudiantes/src/Ventana/UserManager.java
+++ ListaEstudiantes/src/Ventana/UserManager.java
@@ -7,39 +7,52 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 public class UserManager {
     
     private ConectionSql conn;
-    private Statement declaración = null;
+//    private Statement declaración = null;
     private PreparedStatement miSentencia = null;
     private ResultSet resultado = null;
-    private String buscarApp;
     
     public UserManager(ConectionSql newConectionSql){
         conn = newConectionSql;
     }
 
-    public PreparedStatement ConnectedQueryPrepared(String consulta){
+    public void ConnectedQueryPrepared(String consulta){
         try {
-            return conn.getConnection().prepareStatement(consulta);
+            miSentencia = conn.getConnection().prepareStatement(consulta);
         } catch (Exception e) {
-            System.out.println("Error en la consulta! "+e.getMessage());
+            MessageEmergent("ConnectedQueryPrepared() fail "+e.getMessage());
+        }
+    }
+    
+//    public ResultSet PreparedStatementQuery(String buscarapp){
+//        try {
+//            ConnectedQueryPrepared("select cargo, nombre, apellido, sueldo from dato_empleado where apellido=?");
+//            miSentencia.setString(1, buscarApp);
+//            return  miSentencia.executeQuery();
+//        } catch (Exception e) {
+//            System.out.println("Error en respuesta Busqueda! "+e.getMessage());
+//        }
+//        return null;
+//    }
+    
+    private Statement ConnectWithAllUsers() {
+        try {
+            return conn.getConnection().createStatement();
+        } catch (Exception e) {
+            MessageEmergent("ConnectWithAllUsers() fail "+e.getMessage());
         }
         return null;
     }
-        
-//    public Statement state() {
-//        return declaración;
-//    }
     
-    public ResultSet PreparedStatementQuery(String buscarapp){
+    private ResultSet ExecuteConnectionQuery(String query) {
         try {
-            ConnectedQueryPrepared("select cargo, nombre, apellido, sueldo from dato_empleado where apellido=?");
-            miSentencia.setString(1, buscarApp);
-            return  miSentencia.executeQuery();
+            return ConnectWithAllUsers().executeQuery(query);
         } catch (Exception e) {
-            System.out.println("Error en respuesta Busqueda! "+e.getMessage());
+            MessageEmergent("ExecuteConnectionQuery() fail "+e.getMessage());
         }
         return null;
     }
@@ -47,35 +60,15 @@ public class UserManager {
     public ArrayList<Usuario> ShowUsers(){
         ArrayList<Usuario> lista = new ArrayList<Usuario>();
         try {
-            ConnectWithAllUsers();
             resultado = ExecuteConnectionQuery("select * from dato_empleado");
             while(resultado.next()){
                 lista.add(new Usuario(resultado.getString(1), resultado.getString(2), resultado.getString(3), resultado.getString(4), resultado.getInt(5)));
             }
             return lista;
         } catch (Exception e) {
-            System.out.println("error en la coneccion usuarios "+e.getMessage());
+            MessageEmergent("ShowUsers() fail "+e.getMessage());
         }
         return lista;
-    }
-    
-    private void ConnectWithAllUsers() {
-        try {
-//            System.out.println("ppppppppp"+conn);
-            declaración = conn.getConnection().createStatement();
-        } catch (Exception e) {
-            System.out.println("error en el metodo ConnectWithAllUsers()");
-            System.out.println("ooooooo"+e.getMessage());
-        }
-    }
-    
-    private ResultSet ExecuteConnectionQuery(String query) {
-        try {
-            return declaración.executeQuery(query);
-        } catch (Exception e) {
-            System.out.println("Error en las consultas."+e.getMessage());
-        }
-        return null;
     }
     
     public void AddUser(String id_usuario,String cargo,String nombre,String app,int salario) {
@@ -89,9 +82,9 @@ public class UserManager {
             miSentencia.setString(4, app);
             miSentencia.setInt(5, salario);
             miSentencia.executeUpdate();
-            System.out.println("usuario guardado con exito");            
+            MessageEmergent("Add user");
         } catch (Exception e) {
-            System.out.println("error en la adicion de usuarios"+e.getMessage());
+            MessageEmergent("AddUser() fail "+e.getMessage());
         }
     } 
     
@@ -99,35 +92,39 @@ public class UserManager {
         try {
             ConnectedQueryPrepared("DELETE FROM dato_empleado where id_usuario=?");
             miSentencia.setString(1, deleteUser);
-            System.out.println("Eliminado con exito");
             miSentencia.executeUpdate();
+            MessageEmergent("User Deleted");
         } catch (Exception e) {
-            System.out.println("error en la eliminacion del usuario"+e.getMessage());
+            MessageEmergent("RemoveUser() fail"+e.getMessage());
         }
     }
     
-//    public void EditUser(String id_usuarioEdit,String id_usuario,String cargo,String nombre,String app,int salario){
-//        ConnectedQueryPrepared("UPDATE dato_empleado SET id_usuario= ?, cargo= ?, nombre= ?, apellido= ?, sueldo= ? WHERE id_usuario= ?");
-//        try {
-//            miSentencia.setString(1, id_usuario);
-//            miSentencia.setString(2, cargo);
-//            miSentencia.setString(3, nombre);
-//            miSentencia.setString(4, app);
-//            miSentencia.setInt(5, salario);
-//            miSentencia.setString(6, id_usuarioEdit);
-//            miSentencia.executeUpdate();
+    public void EditUser(String id_usuarioEdit,String id_usuario,String cargo,String nombre,String app,int salario){
+        try {
+            ConnectedQueryPrepared("UPDATE dato_empleado SET id_usuario= ?, cargo= ?, nombre= ?, apellido= ?, sueldo= ? WHERE id_usuario= ?");
+            miSentencia.setString(1, id_usuario);
+            miSentencia.setString(2, cargo);
+            miSentencia.setString(3, nombre);
+            miSentencia.setString(4, app);
+            miSentencia.setInt(5, salario);
+            miSentencia.setString(6, id_usuarioEdit);
+            miSentencia.executeUpdate();
 //            System.out.println("usuario editado con exito");
-//        } catch (Exception e) {
-//            System.out.println("error en la edicion de usuario"+e.getMessage());
+            MessageEmergent("Usuario editado con exito");
+        } catch (Exception e) {
+            MessageEmergent("EditUser() fail "+e.getMessage());
+        }
+    }
+    
+    public void MessageEmergent(String message){
+        JOptionPane.showMessageDialog(null, message);
+    }
+    
+//    public static void main (String[] args){
+//        UserManager usermanager = new UserManager(new ConectionSql("root", "root", "jdbc:mysql://localhost:3306/USUARIO"));
+//        usermanager.AddUser("11", "jefe", "aquiles", "vailo", 18);
+//        for (Usuario usuario : usermanager.ShowUsers()) {
+//            System.out.println(usuario.VerInfo());
 //        }
 //    }
-    
-    public static void main (String[] args){
-        
-        UserManager usermanager = new UserManager(new ConectionSql("root", "root", "jdbc:mysql://localhost:3306/USUARIO"));
-        
-        for (Usuario usuario : usermanager.ShowUsers()) {
-            System.out.println(usuario.VerInfo());
-        }
-    }
 }
