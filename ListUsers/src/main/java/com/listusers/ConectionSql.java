7c35733cbac75802343b81b08a6f8167c08d16c2
edwinpaye <baratheonts@gmail.com>
2018-08-17 22:48:07 -0400

    clase ConectionSql

--- ListUsers/src/main/java/com/listusers/ConectionSql.java
+++ ListUsers/src/main/java/com/listusers/ConectionSql.java
@@ -1,5 +1,34 @@
 package com.listusers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConectionSql {
+    
+    private String user;
+    private String password;
+    private String directionIp;
+            
+    public ConectionSql(String user,String password, String directionIp) {
+        this.user=user;
+        this.password = password;
+        this.directionIp = directionIp;
+    }
+    
+    public Connection getConnection(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection(directionIp,user,password);
+        }
+        catch(SQLException e){
+           System.out.println(e);
+        }catch(ClassNotFoundException e){
+           System.out.println(e);
+        }catch(Exception e){
+           System.out.println(e);
+        }
+        return null;
+    }
     
 }
