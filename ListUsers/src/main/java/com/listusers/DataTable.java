392b83db906773cc4c8bf6f7413756ae23d70b1d
edwinpaye <baratheonts@gmail.com>
2018-08-19 23:50:25 -0400

    add metod getTable

--- ListUsers/src/main/java/com/listusers/DataTable.java
+++ ListUsers/src/main/java/com/listusers/DataTable.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.listusers;
 
-/**
- *
- * @author rolando
- */
 public class DataTable {
     
+    private javax.swing.JScrollPane jScrollPane;
+    private javax.swing.JTable jTable;
+
+    public DataTable(javax.swing.JScrollPane newJScrollPane, javax.swing.JTable newJTable){
+        jScrollPane = newJScrollPane;
+        jTable = newJTable;
+    }
+    
+    public javax.swing.JTable getTable(){
+        jTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane.setViewportView(jTable);
+
+        return jTable;
+    }    
 }
